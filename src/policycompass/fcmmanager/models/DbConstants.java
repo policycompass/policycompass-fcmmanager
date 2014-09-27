ae0a205a59d6f89c65a26de80625c8bf3b80c7ca
Nauman Riaz Chaudhry <nauman.riaz@gmail.com>
2014-09-27 13:20:31 +0100

    FCM Manager Web Service updated

---
+++ src/policycompass/fcmmanager/models/DbConstants.java
@@ -0,0 +1,11 @@
+package policycompass.fcmmanager.models;
+
+public class DbConstants {
+
+	public static final String Host = "localhost";
+	public static final String Port = "5432";
+	public static final String Database = "pcompass";
+	public static final String UserName = "pcompass";
+	public static final String Password = "ALLAHisG8";
+	
+}
