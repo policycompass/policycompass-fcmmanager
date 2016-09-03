c0cf1a62db882bcbedf46da67690ffc2e01be996
Mohammed Bahja <mohammed.bahja@brunel.ac.uk>
2016-09-03 12:55:17 +0100

    public models query policycompass/policycompass#659

--- src/main/java/policycompass/fcmmanager/controllers/FCMModels.java
+++ src/main/java/policycompass/fcmmanager/controllers/FCMModels.java
@@ -55,7 +55,7 @@ public class FCMModels {
 		Session session = HibernateUtil.getSessionFactory().openSession();
 
 		session.beginTransaction();
-		Query query = session.createQuery("from fcmmanager_models");
+		Query query = session.createQuery("from fcmmanager_models where isdraft=false or isdraft=null"); //Return only public models
 		@SuppressWarnings("unchecked")
 		List<FCMModel> model = query.list();
 		session.clear();
